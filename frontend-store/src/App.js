7a812669f27a4463740bc4547346ca57cc771063
siccoo <obichilaka@gmail.com>
2021-07-09 15:48:31 +0100

    html to jsx changes

---
+++ frontend-store/src/App.js
@@ -0,0 +1,206 @@
+
+
+function App() {
+  return (
+    <div classNameName="grid__container">
+      <header className="row">
+          <div>
+              <a className="brand" href="index.html">Amazona</a>
+          </div>
+          <div>
+              <a href="cart.html">Cart</a>
+              <a href="signin.html">Sign In</a>
+          </div>
+      </header>
+      <main>
+          <div className="row center">
+              <div className="card">
+                  <a href="product.html">
+                      <img className="medium" src="./images/ps5.jpg" alt="product" />
+                  </a>
+                  <div className="card__body">
+                      <a href="product.html">
+                          <h2>Play Station 5</h2>
+                      </a>
+                      <div className="rating">
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                      </div>
+                      <div className="price">
+                          ₦ 100,000.00
+                      </div>
+                  </div>
+              </div>
+              <div className="card">
+                  <a href="product.html">
+                      <img className="medium" src="./images/ps5.jpg" alt="product" />
+                  </a>
+                  <div className="card__body">
+                      <a href="product.html">
+                          <h2>Play Station 5</h2>
+                      </a>
+                      <div className="rating">
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                      </div>
+                      <div className="price">
+                          ₦ 100,000.00
+                      </div>
+                  </div>
+              </div>
+              <div className="card">
+                  <a href="product.html">
+                      <img className="medium" src="./images/ps5.jpg" alt="product" />
+                  </a>
+                  <div className="card__body">
+                      <a href="product.html">
+                          <h2>Play Station 5</h2>
+                      </a>
+                      <div className="rating">
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                      </div>
+                      <div className="price">
+                          ₦ 100,000.00
+                      </div>
+                  </div>
+              </div>
+              <div className="card">
+                  <a href="product.html">
+                      <img className="medium" src="./images/ps5.jpg" alt="product" />
+                  </a>
+                  <div className="card__body">
+                      <a href="product.html">
+                          <h2>Play Station 5</h2>
+                      </a>
+                      <div className="rating">
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                      </div>
+                      <div className="price">
+                          ₦ 100,000.00
+                      </div>
+                  </div>
+              </div>
+              <div className="card">
+                  <a href="product.html">
+                      <img className="medium" src="./images/ps5.jpg" alt="product" />
+                  </a>
+                  <div className="card__body">
+                      <a href="product.html">
+                          <h2>Play Station 5</h2>
+                      </a>
+                      <div className="rating">
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                      </div>
+                      <div className="price">
+                          ₦ 100,000.00
+                      </div>
+                  </div>
+              </div>
+              <div className="card">
+                  <a href="product.html">
+                      <img className="medium" src="./images/ps5.jpg" alt="product" />
+                  </a>
+                  <div className="card__body">
+                      <a href="product.html">
+                          <h2>Play Station 5</h2>
+                      </a>
+                      <div className="rating">
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                          <span>
+                              <i className="fa fa-star"></i>
+                          </span>
+                      </div>
+                      <div className="price">
+                          ₦ 100,000.00
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </main>
+      <footer className="row center">
+        All rights reserved
+      </footer>
+    </div>
+  );
+}
+
+export default App;
