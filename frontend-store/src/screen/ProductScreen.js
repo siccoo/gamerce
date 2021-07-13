430696264968e37bfe195c84bdcec8c696b3187e
siccoo <obichilaka@gmail.com>
2021-07-13 12:36:14 +0100

    updates on ProductScreen

--- frontend-store/src/screen/ProductScreen.js
+++ frontend-store/src/screen/ProductScreen.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { data } from '../data';
+import Rating from "../component/Rating";
 
-const ProductScreen = () => {
+const ProductScreen = (props) => {
+    const product = data.products.find((x) => x._id === props.match.params.id );
+    if(!product) {
+        return <div>Product Not Found</div>;
+    }
     return (
         <div>
-            Product Screen
+            <div className="row">
+                <div className="col-2">
+                    <img className="large" src={product.image} alt={product.name} />
+                </div>
+                <div className="col-1">
+                    <ul>
+                        <li>
+                            <h1>{product.name}</h1>
+                        </li>
+                        <li>
+                            <Rating
+                                rating={product.rating}
+                                numReviews={product.numReviews} />
+                        </li>
+                        <li>Price: ₦{product.price}</li>
+                        <li>Description {product.description}</li>
+                    </ul>
+                </div>
+                <div className="col-1">
+
+                </div>
+            </div>
         </div>
     )
 }
