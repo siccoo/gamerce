4a13f116fccc0e22bfd297ae8f5c91bb6619ab63
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-08-25 13:50:59 +0100

    cartItem badge

--- frontend-store/src/App.js
+++ frontend-store/src/App.js
@@ -1,9 +1,13 @@
+import { useSelector } from "react-redux";
 import { BrowserRouter, Link, Route } from "react-router-dom";
 import CartScreen from "./screen/CartScreen";
 import HomeScreen from "./screen/HomeScreen";
 import ProductScreen from "./screen/ProductScreen";
-
+ 
 function App() {
+  const cart = useSelector(state => state.cart);
+  const { cartItems } = cart;
+
   return (
     <BrowserRouter>
     <div className="grid__container">
@@ -12,7 +16,11 @@ function App() {
               <Link className="brand" to="/">Amazona</Link>
           </div>
           <div>
-              <Link to="/cart">Cart</Link>
+              <Link to="/cart">Cart
+                {cartItems.length > 0 && (
+                  <span className="badge">{cartItems.length}</span>
+                )}
+              </Link>
               <Link to="/signin">Sign In</Link>
           </div>
       </header>
