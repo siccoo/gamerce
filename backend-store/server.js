fd80e0dec92f2f87dd45ef3c36837d962dca6004
Chilaka Michael Obinna <obichilaka@gmail.com>
2021-09-07 10:16:43 +0100

    connect mongoose in  server.js

--- backend-store/server.js
+++ backend-store/server.js
@@ -1,7 +1,14 @@
 import express from "express";
+import mongoose from "mongoose";
 import data from "./data.js";
 
 const app = express();
+
+mongoose.connect("mongodb://localhost/gamehouse", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+});
 
 app.get("/api/products/:id", (req, res) => {
     const product = data.products.find((x) => x._id === req.params.id);
